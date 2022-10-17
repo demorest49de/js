abd57af5991c0f963ae536b9ec20fbc3531f0583
shevchenkoak <demorest49de@gmail.com>
2022-10-17 21:00:25 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const name = 'xiaomi 12t pro';
+const quantity = 5;
+const category = 'phones';
+const price = 50000;
+
+console.log('имя: ', name);
+console.log('всего товара на сумму: ', quantity * price);
