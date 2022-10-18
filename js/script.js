aca4d491fb6ba83f3d7a927ea44f392d23946244
shevchenkoak <demorest49de@gmail.com>
2022-10-18 13:43:29 +0300

    C

--- js/script.js
+++ js/script.js
@@ -1,9 +1,88 @@
 'use strict';
+// region primitives
+// const name = 'xiaomi 12t pro';
+// const quantity = 5;
+// const category = 'phones';
+// const price = 50000;
+//
+// console.log('имя: ', name);
+// console.log('всего товара на сумму: ', quantity * price);
 
-const name = 'xiaomi 12t pro';
-const quantity = 5;
-const category = 'phones';
-const price = 50000;
 
-console.log('имя: ', name);
-console.log('всего товара на сумму: ', quantity * price);
+// console.log('Numbers: ', 5, 3.5, Infinity, -Infinity, NaN);
+//
+// console.log('bigInt: ', 10n, BigInt(10));
+//
+// console.log('Strings: ', 'one', "two", `three`);
+//
+// console.log('Логический: ', true, false);
+//
+// console.log('undefined: ', undefined);
+//
+// console.log('Symbol: ', Symbol(), Symbol('key'));
+
+// console.log(': ', typeof 5.5);
+// console.log(': ',typeof (NaN));
+// console.log(': ',typeof (Infinity));
+//
+// console.log(': ',Number.MAX_VALUE);
+// console.log(': ',Number.MAX_SAFE_INTEGER);
+
+
+// const myName = 'Max'
+// console.log(`My name is ${myName}`);
+//
+// const birthYear = 1981;
+// const nowYear = new Date().getFullYear();
+
+
+//  тики - обратный опостроф + интерполяция
+// document.body.innerHTML = `<h1 class="title">Hi ${myName}</h1>
+// <p>I'm ${nowYear - birthYear} years old</p>
+// `;
+// endregion
+
+//region objects
+// const obj = {
+//     num: 5,
+//     str: 'str',
+//     bool: true,
+//     foo() {
+//     },
+//
+//     arr: [1, 2, 3],
+//     obj: {
+//         a: 1,
+//         b: 2
+//     }
+//
+// };
+// console.log(': ', typeof (obj));
+//
+// const arr = [[1, 2, 3], {a: 1, b: 2}, 'str', 4, false, () => {
+// }];
+// console.log(': ', typeof (arr));
+//
+// const foo = () => {
+// };
+//
+// console.log(': ', typeof (foo));
+//
+// const err = new Error();
+// console.log(': ', typeof (err));
+//
+// const regExp = /А-Я/i;
+// console.log(': ',typeof (regExp));
+//endregion
+
+var one = 1;
+var one = 1;
+var one = 1;
+var one = 1;
+var one = 1;
+
+let two = 2;
+two = 'two'
+const three = 3;
+
+console.log(': ',one, two, three);
