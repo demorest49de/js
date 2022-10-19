d63bafcdbae757d792f391ca730978b7bf5e2299
shevchenkoak <demorest49de@gmail.com>
2022-10-19 18:42:15 +0300

    C

---
+++ js.4-02/js/einstein.js
@@ -0,0 +1,5 @@
+const myWeight = prompt('Введите вашу массу тела (или кота)75');
+const speedLight = 3 * 10 ** 8
+
+console.log(`Масса вашего тела: `, myWeight);
+console.log('Энергия вашего тела по формуле E = mc^2: ', myWeight * speedLight ** 2);
---
+++ js.4-02/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const name = prompt(`Введите наименование товара`)
+const quantity = prompt(`Введите количество товара`)
+const category = prompt(`Введите катергорию товара`)
+const price = prompt(`Введите цену товара`)
+
+console.log(`тип данных: количество - ${typeof (+quantity)} и цены - ${typeof (+price)}`);
+console.log(`На складе ${quantity} единицы товара "${name}" на сумму ${quantity * price} деревянных`);
