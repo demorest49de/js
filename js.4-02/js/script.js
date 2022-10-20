58e1445447e3a5f1c3fc11d88cae382bbb6ea53e
shevchenkoak <demorest49de@gmail.com>
2022-10-20 17:29:56 +0300

    с

--- js.4-02/js/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const name = prompt(`Введите наименование товара`)
-const quantity = +prompt(`Введите количество товара`)
-const category = prompt(`Введите катергорию товара`)
-const price = +prompt(`Введите цену товара`)
-
-console.log(`тип данных: количество - ${typeof (quantity)} и цены - ${typeof (price)}`);
-console.log(`На складе ${quantity} единицы товара "${name}" на сумму ${quantity * price} деревянных`);
