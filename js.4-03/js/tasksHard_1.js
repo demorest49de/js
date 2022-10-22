e0093eb468c1b411ccd3a552f5c523281ee72aa0
shevchenkoak <demorest49de@gmail.com>
2022-10-22 15:05:44 +0300

    с

--- js.4-03/js/tasksHard_1.js
+++ js.4-03/js/tasksHard_1.js
@@ -6,9 +6,9 @@ if(isNaN(income)){
     if(income > 0 && income <= 15000){
         console.log(`Сумма налога по доходу ${income} по ставке 13% состовляет ${income * 0.13}`)
     }
-    else if(income > 15000 && income <= 50000){
+    else if(income <= 50000){
         console.log(`Сумма налога по доходу ${income} по ставке 20% состовляет ${income * 0.2}`)
-    }else if(income > 50000){
+    }else{
         console.log(`Сумма налога по доходу ${income} по ставке 30% состовляет ${income * 0.3}`)
     }
 }
