c5644806665c2afe10091921ab5697ec3e1a4139
shevchenkoak <demorest49de@gmail.com>
2022-10-22 15:33:19 +0300

    с

--- js.4-03/js/tasksHard_1.js
+++ js.4-03/js/tasksHard_1.js
@@ -1,14 +1,11 @@
 let income = +prompt('Введите сумму вашего дохода');
 
-if(isNaN(income)){
-    income = +prompt('Вы ввели неправильное значение.')
-} else{
-    if(income > 0 && income <= 15000){
-        console.log(`Сумма налога по доходу ${income} по ставке 13% состовляет ${income * 0.13}`)
-    }
-    else if(income <= 50000){
-        console.log(`Сумма налога по доходу ${income} по ставке 20% состовляет ${income * 0.2}`)
-    }else{
-        console.log(`Сумма налога по доходу ${income} по ставке 30% состовляет ${income * 0.3}`)
-    }
+
+if (income <= 15000) {
+    console.log(`Сумма налога по доходу ${income} по ставке 13% состовляет ${income * 0.13}`);
+} else if (income <= 50000) {
+    console.log(`Сумма налога по доходу ${income} по ставке 20% состовляет ${income * 0.2}`);
+} else {
+    console.log(`Сумма налога по доходу ${income} по ставке 30% состовляет ${income * 0.3}`);
 }
+
