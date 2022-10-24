562632634974b5806ec1dd233d4e928654027c13
shevchenkoak <demorest49de@gmail.com>
2022-10-24 12:33:57 +0300

    c

--- js.4-04/js/Task01.js
+++ js.4-04/js/Task01.js
@@ -6,7 +6,7 @@ const evaluateConversion = (euro, eurToUsd) => {
     return priceInRubles;
 };
 
-const userInput = +prompt('Введите стоимость покупки в евро:');
+const userInput1 = +prompt('Введите стоимость покупки в евро:');
 const eurUsdCrossCurs = 1.2;
-const result = evaluateConversion(userInput, (value) => value * eurUsdCrossCurs);
-console.log('result: ', result);
+const result1 = evaluateConversion(userInput1, (value) => value * eurUsdCrossCurs);
+console.log('result: ', result1);
--- js.4-04/js/Task02.js
+++ js.4-04/js/Task02.js
@@ -3,6 +3,6 @@ const changeCapitalLetter = (phrase) => {
     return toLower.charAt(0).toUpperCase() + toLower.slice(1);
 };
 
-userInput = prompt('Введите фразу:');
+const userInput2 = prompt('Введите фразу:');
 
-result = console.log('result: ', changeCapitalLetter(userInput));
+const result2 = console.log('result: ', changeCapitalLetter(userInput2));
