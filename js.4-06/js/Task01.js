f21bd68323f85a07b587f3393f6216a3bac889be
shevchenkoak <demorest49de@gmail.com>
2022-10-26 11:10:09 +0300

    с

--- js.4-06/js/Task01.js
+++ js.4-06/js/Task01.js
@@ -1,9 +1,18 @@
 'use strict';
 
-const filter = ([...all],[...failed]) => {
+const filter = ([...students], failed) => {
 
+    for (const failedStudent of failed) {
+        if (students.includes(failedStudent)) {
+            const index = students.indexOf(failedStudent);
+            students.splice(index, 1);
+        }
+    }
+    return students;
 };
 
 const allStud = ['Ivanov', 'Petrov', 'Sidorov', 'Kuznecov', 'Cmirnov', 'Vasechkin'];
 const failedStudents = ['Petrov', 'Kuznecov', 'Cmirnov'];
 
+const successStudents = filter(allStud, failedStudents)
+console.log(': ',successStudents);
---
+++ js.4-06/js/Task03.js
@@ -0,0 +1,6 @@
+const addPrefix = (array, prefix) => {
+    const
+};
+
+const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+const namesWithPrefix = addPrefix(names, 'Mr')
