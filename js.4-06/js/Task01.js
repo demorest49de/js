821fe30db9e8bba9afac6947fd0bdfb76f561b6b
shevchenkoak <demorest49de@gmail.com>
2022-10-26 07:54:54 +0300

    с

--- js.4-06/js/Task01.js
+++ js.4-06/js/Task01.js
@@ -1,7 +1,9 @@
 'use strict';
 
-for (let i = 1; i <= 10; i++) {
-    for (let j = 1; j <= 10; j++) {
-        console.log(`${i} ^ ${j} = ${i ** j}`);
-    }
-}
+const filter = ([...all],[...failed]) => {
+
+};
+
+const allStud = ['Ivanov', 'Petrov', 'Sidorov', 'Kuznecov', 'Cmirnov', 'Vasechkin'];
+const failedStudents = ['Petrov', 'Kuznecov', 'Cmirnov'];
+
