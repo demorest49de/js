7a89e7a9d754ce8826708d76b23f9c08b8cf13e1
shevchenkoak <demorest49de@gmail.com>
2022-10-28 15:07:49 +0300

    c

--- js.4-07/js/Task02.js
+++ js.4-07/js/Task02.js
@@ -20,27 +20,6 @@ const randomArrayV2 = (length = 1, n = 1, m = 1, parity) => {
         array[i] = Math.trunc(Math.random() * (max - min) + min);
     }
 
-    if (parity === 'even') {
-        const evenNumbers = [];
-        array.forEach((item) => {
-            if (!(item % 2)) {
-                evenNumbers.push(item);
-            }
-        });
-
-        return evenNumbers;
-    } else if (parity === 'odd') {
-        const oddNumbers = [];
-
-        array.forEach((item) => {
-            if (item % 2) {
-                oddNumbers.push(item);
-            }
-        });
-
-        return oddNumbers;
-    }
-
     return array;
 };
 
---
+++ js.4-07/js/Task03.js
@@ -0,0 +1,47 @@
+const randomArrayV3 = (length = 1, n = 1, m = 1, parity) => {
+
+    n = Math.abs(n);
+    m = Math.abs(m);
+    let min = 0;
+    let max = 0;
+
+    if (n < m) {
+        min = n;
+        max = m;
+    } else {
+        min = m;
+        max = n;
+    }
+    console.log(': ', min, max);
+    const array = [];
+    array.length = length;
+
+    for (let i = 0; i < array.length; i++) {
+        array[i] = Math.trunc(Math.random() * (max - min) + min);
+    }
+
+    if (parity === 'even') {
+        const evenNumbers = [];
+        array.forEach((item) => {
+            if (!(item % 2)) {
+                evenNumbers.push(item);
+            }
+        });
+
+        return evenNumbers;
+    } else if (parity === 'odd') {
+        const oddNumbers = [];
+
+        array.forEach((item) => {
+            if (item % 2) {
+                oddNumbers.push(item);
+            }
+        });
+
+        return oddNumbers;
+    }
+
+    return array;
+};
+
+console.log(': ', randomArrayV3(25, -65, -33, 'odd'));
