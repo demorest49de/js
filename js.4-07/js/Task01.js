ee75455d8749bd17443edad3a6f136225c58504a
shevchenkoak <demorest49de@gmail.com>
2022-10-28 13:28:56 +0300

    c

--- js.4-07/js/Task01.js
+++ js.4-07/js/Task01.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const randomArray = (quantity) => {
+    const array = [];
+    array.length = quantity;
+    for (let i = 0; i < array.length; i++) {
+        array[i] = Math.trunc(Math.random() * 100) + 1;
+    }
+
+    return array;
+};
+
+const result = randomArray(150);
+console.log(': ', result);
