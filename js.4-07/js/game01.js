aea28422c795a2bc3cd2e9d0caa3da21fde45ca3
shevchenkoak <demorest49de@gmail.com>
2022-10-29 12:32:41 +0300

    c

--- js.4-07/js/game01.js
+++ js.4-07/js/game01.js
@@ -1,23 +1,27 @@
-alert(`Угадай число`);
-
 const guessNumber = () => {
-    return Math.trunc(Math.random() * 100 + 1);
+    alert(`Угадай число`);
+
+    const getRandom = () => {
+        return Math.trunc(Math.random() * 100 + 1);
+    };
+
+    const secretNumber = getRandom();
+
+    let userGuess = prompt('Введите загаданное число от 1 до 100');
+    console.log(': ', userGuess);
+    while (secretNumber != userGuess) {
+        if (userGuess === null) return;
+        while (isNaN(userGuess)) {
+            userGuess = prompt(`Введите число!`);
+        }
+        if (userGuess > secretNumber) {
+            userGuess = prompt(`Меньше! Попробуйте еще раз!`);
+        } else {
+            userGuess = prompt(`Больше! Попробуйте еще раз!`);
+        }
+    }
+
+    alert('Правильно!');
 };
 
-const secretNumber = guessNumber();
-
-let userGuess = +prompt('Введите загаданное число от 1 до 100');
-
-while (secretNumber !== userGuess) {
-    while(isNaN(userGuess)){
-        userGuess = +prompt(`Введите число!`);
-    }
-    if (userGuess > secretNumber) {
-        userGuess = +prompt(`Меньше! Попробуйте еще раз!`);
-    } else {
-        userGuess = +prompt(`Больше! Попробуйте еще раз!`);
-    }
-
-}
-
-alert('Правильно!');
+guessNumber();
