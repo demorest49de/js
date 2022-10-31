e9ba13dc745ba20c96414791d0981d984dd32c67
shevchenkoak <demorest49de@gmail.com>
2022-10-31 10:49:58 +0300

    c

--- js.4-07/js/Task03.js
+++ js.4-07/js/Task03.js
@@ -9,12 +9,16 @@ const randomArrayV3 = (length, n, m, parity = '') => {
     min -= 1;
 
     for (let i = 0; i < array.length; i++) {
+
+
+
         array[i] = Math.trunc((Math.random() * (max - min)) + min);
 
         if (array[i] === 0) {
-            // array[i] = Math.abs(array[i]);
             continue;
         }
+
+
     }
 
     if(parity === ''){
