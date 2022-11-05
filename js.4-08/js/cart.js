33181852d01f12524abd6dc06b5b640f7d5a847a
shevchenkoak <demorest49de@gmail.com>
2022-11-05 11:47:38 +0300

    c

---
+++ js.4-08/js/cart.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const cart = {
+    items: [],
+    totalPrice: 0,
+    count: 0,
+    getTotalPrice() {
+        return this.totalPrice;
+    },
+    add(name, price, amount = 1) {
+        const product = {
+            name,
+            price,
+            amount,
+        };
+        this.items.push(product);
+        this.increaseCount(amount);
+        this.calculateItemPrice();
+    },
+    increaseCount(amount) {
+        this.count += amount;
+    },
+    calculateItemPrice() {
+        let totalForAllProducts = 0;
+        for (const item of this.items) {
+            totalForAllProducts += item.price * item.amount;
+        }
+        this.totalPrice = totalForAllProducts;
+    },
+    clear() {
+        this.items = [];
+        this.totalPrice = 0;
+        this.count = 0;
+    },
+    print() {
+        console.log(JSON.stringify(this.items));
+        console.log(`Общая стоимость корзины: ${this.totalPrice} рублей`);
+    },
+};
+
+cart.add('red wine', 2000, 3);
+cart.add('white wine', 1000, 1);
+cart.add('parmesane', 300, 1);
+cart.add('black angus', 1500, 2);
+
+cart.print();
+
