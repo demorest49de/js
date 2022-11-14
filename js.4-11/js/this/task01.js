15c2e3642e8e140d0867b2f2d9902370a06d607a
shevchenkoak <demorest49de@gmail.com>
2022-11-14 15:33:54 +0300

    c

---
+++ js.4-11/js/this/task01.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const rectangle = {
+  width: 5,
+  height: 5,
+  get Width() {
+    return this.width + 'см';
+  },
+  set Width(value) {
+    if (typeof (value) === 'number') {
+      this.width = value;
+    } else {
+      console.log(`${value} is not a number`);
+    }
+  },
+  get Height() {
+    return this.height + 'см';
+  },
+  set Height(value) {
+    if (typeof (value) === 'number') {
+      this.height = value;
+    } else {
+      console.log(`${value} is not a number`);
+    }
+  },
+};
+
+rectangle.Width = 'alsgfj';
+rectangle.Width = 111;
+
+rectangle.Height = 45;
+console.log(rectangle.Width);
+console.log(rectangle.Height);
+
