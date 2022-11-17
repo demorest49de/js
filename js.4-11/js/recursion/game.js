f550b21c45358cc26d524fe8ce43dfc2728d68f2
shevchenkoak <demorest49de@gmail.com>
2022-11-17 12:48:05 +0300

    c

--- js.4-11/js/recursion/game.js
+++ js.4-11/js/recursion/game.js
@@ -1,32 +1,35 @@
 'use strict';
-alert(`Угадай число`);
+// alert(`Угадай число`);
 
 const guessHandler = () => {
   const secretNumber = Math.trunc(Math.random() * 100 + 1);
+
   console.log(': secretNumber', secretNumber);
   const guessNumber = (response) => {
+
     console.log(': ', response);
-    console.log(': ', typeof(response));
+    console.log(': ', typeof (response));
 
-    if (response === null) {
-      alert('Выход из программы...');
-      return;
-    } else if (isNaN(response) || +response === 0) {
-      response = prompt(`Введите число!`);
-      return guessNumber(response);
-    }
 
-    switch (+response === secretNumber) {
-      case true:
-        alert(`Вы угадали число ${response}!`);
-        return;
+    switch (true) {
+      case response === null:
+        return alert('Выход из программы...');
 
-      case false:
-        response = response > secretNumber ?
-          prompt(`Меньше! Попробуйте еще раз!`) :
-          prompt(`Больше! Попробуйте еще раз!`);
-        guessNumber(response);
-        return;
+      case isNaN(response) || +response === 0:
+        response = prompt(`Введите число!`);
+        return guessNumber(response);
+
+      case response > secretNumber:
+         response = prompt(`Меньше! Попробуйте еще раз!`);
+        return guessNumber(response);
+
+      case  response < secretNumber:
+        response = prompt(`Больше! Попробуйте еще раз!`);
+        return guessNumber(response);
+
+      default:
+        return alert(`Вы угадали число ${response}!`);
+
     }
   };
   guessNumber(prompt('Введите загаданное число от 1 до 100'));
--- js.4-11/js/recursion/recursion.js
+++ js.4-11/js/recursion/recursion.js
@@ -3,22 +3,23 @@
 const array = [];
 
 
-const arrayCheckerHandler = (sum = 0) => {
+const createArray = () => {
   const random = Math.trunc(Math.random() * 10);
   let sumCounter = sum;
   sumCounter += random;
 
-  const checkSum = (sum, random) => {
-    if (sum <= 50) {
-      array.push(random);
 
-      arrayCheckerHandler(sum);
-    }
-    return [...array];
-  };
 
   return checkSum(sumCounter, random);
 };
 
+const checkSum = (sum, random) => {
+  if (sum <= 50) {
+    array.push(random);
 
-console.log(arrayCheckerHandler());
+    createArray();
+  }
+  return [...array];
+};
+
+console.log(createArray());
