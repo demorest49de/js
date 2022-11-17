ed355565dcafa417223d10d3a0549f4a71443715
shevchenkoak <demorest49de@gmail.com>
2022-11-17 17:33:13 +0300

    c

--- js.4-11/js/recursion/recursion.js
+++ js.4-11/js/recursion/recursion.js
@@ -1,25 +1,29 @@
 'use strict';
 
-const array = [];
+const array = [8, 3, 1, 2];
 
 
-const createArray = () => {
-  const random = Math.trunc(Math.random() * 10);
-  let sumCounter = sum;
-  sumCounter += random;
-
-
-
-  return checkSum(sumCounter, random);
+const checkArraySum = (sum, [...rest]) => {
+  const [x] = rest;
+  sum += x;
+  if (sum < 50 && rest.length > 1) {
+    rest.shift();
+    sum = checkArraySum(sum, rest);
+  }
+  return sum;
 };
 
-const checkSum = (sum, random) => {
+const checkSum = (sum, [...array]) => {
+  const random = Math.trunc(Math.random() * 10);
+  sum += random;
   if (sum <= 50) {
     array.push(random);
-
-    createArray();
+    array = checkSum(sum, array);
   }
-  return [...array];
+  return array;
 };
 
-console.log(createArray());
+const sum = checkArraySum(0, array);
+
+const result = checkSum(sum, array);
+console.log(': ', result);
