c1a97d2daf5c119c00824ae5b17aed168d20c98f
shevchenkoak <demorest49de@gmail.com>
2022-11-23 13:25:20 +0300

    c

--- js.4-12/js/game.js
+++ js.4-12/js/game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(() => {
+window.rockPaperScissors = (() => {
   const FIGURES_ENG = ['rock', 'scissors', 'paper'];
   const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
 
@@ -41,8 +41,17 @@
       computer: 0,
     };
 
-    const addSpace = (lang) => lang.map((item, index) => (index !== 0 ? ' ' + item : item));
+    language = language.toUpperCase();
+    const lang = language === 'EN' || language === 'ENG' ?
+      FIGURES_ENG : FIGURES_RUS;
 
+    const viewLang = language === 'EN' || language === 'ENG' ?
+      viewLangENG : viewLangRUS;
+
+    const addSpace = (lang) => lang.map((item, index) =>
+      (index !== 0 ? ' ' + item : item));
+
+    const arrayWithSpaces = addSpace([...lang]);
 
     const parseResponse = (str) => {
       if (str === null) return null;
@@ -73,8 +82,6 @@
       const rules = getRules(lang);
       const result = response !== figure ? [response, figure] : [null];
 
-      console.log(': ', result);
-
       for (const [ruleArray, value] of rules) {
         if (JSON.stringify(ruleArray) === JSON.stringify(result)) {
           return value;
@@ -83,19 +90,11 @@
     };
 
     const exitMessage = () => {
-      alert(`${viewLang.result}:\n${viewLang.computer} - ${result.computer},\n${viewLang.you} - ${result.player}`);
+      alert(`${viewLang.result}:\n${viewLang.computer}` +
+        ` - ${result.computer},\n${viewLang.you} - ${result.player}`);
     };
-    language = language.toUpperCase();
-    const lang = language === 'EN' || language === 'ENG' ?
-      FIGURES_ENG : FIGURES_RUS;
-
-    const viewLang = language === 'EN' || language === 'ENG' ?
-      viewLangENG : viewLangRUS;
-
-    const arrayWithSpaces = addSpace([...lang]);
 
     return function start() {
-
       const restart = () => {
         const figure = getFigure(lang);
         console.log(': ', figure);
@@ -111,18 +110,21 @@
             return start();
 
           case (getWinner(response, figure).hasWin === null):
-            alert(`${viewLang.computer}: ${figure}\n${viewLang.you}: ${response}\n${viewLang.draw}`);
+            alert(`${viewLang.computer}: ${figure}\n` +
+              `${viewLang.you}: ${response}\n${viewLang.draw}`);
             return start();
 
           case (!getWinner(response, figure).hasWin):
-            alert(`${viewLang.computer}: ${figure}\n${viewLang.you}: ${response}\n${viewLang.youLose}`);
+            alert(`${viewLang.computer}: ${figure}\n` +
+              `${viewLang.you}: ${response}\n${viewLang.youLose}`);
             result.computer += 1;
             console.log('result.computer: ', result.computer);
             response = prompt(`${viewLang.more}?`);
             return response !== null ? start() : exitMessage();
 
           case (getWinner(response, figure).hasWin):
-            alert(`${viewLang.computer}: ${figure}\n${viewLang.you}: ${response}\n${viewLang.youWin}`);
+            alert(`${viewLang.computer}: ${figure}\n` +
+              `${viewLang.you}: ${response}\n${viewLang.youWin}`);
             result.player += 1;
             console.log('result.player: ', result.player);
             response = prompt('Еще?');
@@ -137,5 +139,5 @@
     };
   };
 
-  window.RPC = game;
+  return game;
 })();
--- js.4-12/js/main.js
+++ js.4-12/js/main.js
@@ -1,4 +1,4 @@
 'use strict';
 
-const startGame = window.RPC('en');
+const startGame = window.rockPaperScissors('en');
 startGame();
