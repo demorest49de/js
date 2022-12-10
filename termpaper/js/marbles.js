2e545d64037a112168b669b7685039012b4687f5
shevchenkoak <demorest49de@gmail.com>
2022-12-10 16:33:06 +0300

    c

--- termpaper/js/marbles.js
+++ termpaper/js/marbles.js
@@ -7,8 +7,8 @@ window.marbles = (() => {
       minValue: 0,
       maxValue: 10,
       current: -1,
-      bot: 5,
-      player: 5,
+      bot: 0,
+      player: 1,
       score: [5, 5],
       switchUser() {
         this.current = +!this.current;
@@ -18,36 +18,28 @@ window.marbles = (() => {
       },
       showResult(message) {
         alert(`${message}! 
-        \n${ide.bot} - бот
-        \n${ide.player} - игрок`);
+        \n${ide.score[0]} - бот
+        \n${ide.score[1]} - игрок`);
       },
       changeScore(value) {
         if (value > 0) {
-          ide.bot -= value;
-          ide.player += value;
+          ide.score[0] -= value;
+          ide.score[1] += value;
         } else {
-          ide.bot += Math.abs(value);
-          ide.player -= Math.abs(value);
+          ide.score[1] += Math.abs(value);
+          ide.score[0] -= Math.abs(value);
         }
-        checkScoreConstraints(ide.score);
+        checkScoreConstraints();
       },
     };
 
     const evenOdd = ['четное', 'нечетное'];
-    const checkScoreConstraints = (score) => {
-      for (let i = 0; i < score.length; i++) {
+    const checkScoreConstraints = () => {
+      for (let i = 0; i < ide.score.length; i++) {
         ide.score[i] = ide.score[i] < ide.minValue ? ide.minValue
           : ide.score[i] > ide.maxValue ? ide.maxValue : ide.score[i];
       }
     };
-
-    // const isNumber = (num) => {
-    //   if (num === null) return null;
-    //   if (!Number.isNaN(parseFloat(num)) && isFinite(num)) {
-    //     return +num;
-    //   }
-    //   return undefined;
-    // };
 
     const getRandomIntInclusive = (min, max) => {
       min = Math.ceil(min);
@@ -59,33 +51,33 @@ window.marbles = (() => {
 
       const nextTurn = () => {
         let answer = null;
-        let random = null;
+        const random = getRandomIntInclusive(1, ide.score[ide.current]);
         if (ide.current === 1) {
-          random = getRandomIntInclusive(1, ide.score[ide.current]);
-          answer = prompt(`Сколько шариков из ${ide.player} ты хочешь разыграть?`);
+          answer = prompt(`Сколько шариков из ${ide.score[1]} ты хочешь разыграть?`);
           console.log('bot, user: ', random, +answer);
           if (hasFaultsOrExit(answer)) {
             return;
           }
-          if (+answer > ide.player || +answer <= 0) {
-            alert(`Ошибка! Ты можешь разыграть ${ide.player} шариков. Попробуйте еще раз`);
+          if (+answer > ide.score[1] || +answer <= 0) {
+            alert(`Ошибка! Ты можешь разыграть ${ide.score[1]} шариков. Попробуйте еще раз`);
             return nextTurn();
           }
         } else {
-          random = getRandomIntInclusive(0, 1);
           answer = confirm(`Отгадайте: ${evenOdd.join(' или ')}?`);
           console.log('bot, user: ', random, +answer);
+          // проверка на четность
         }
+        hasExit();
         if (+answer === random) {
-          alert(`Ничья!`)
+          alert(`Ничья!`);
+          return nextTurn();
         } else if (+answer !== random) {
-          ide.changeScore(+answer);
+          ide.changeScore(answer);
           ide.showResult('Вы победили');
         } else {
           ide.changeScore(-(+answer));
           ide.showResult('Вы проиграли');
         }
-        hasExit();
       };
 
       const wantPlayAgain = () => {
@@ -111,16 +103,15 @@ window.marbles = (() => {
         const [x, y] = ide.score;
         if (!x || !y) {
           const message = `Игра окончена. ${x === 0 ? 'Вы победили!' : 'Вы проиграли!' +
-            `\n${ide.bot} - бот` +
-            `\n${ide.player} - игрок`} `;
+            `\n${ide.score[0]} - бот` +
+            `\n${ide.score[1]} - игрок`} `;
           alert(message);
           wantPlayAgain();
         }
       };
       do {
         if (ide.current < 0) {
-          // ide.current = +window.rpc()();
-          ide.current = 1;
+          ide.current = +window.rpc()();
           console.log('ide.score: ', ide.score);
         } else {
           ide.switchUser();
